1c8f8d1e474044427278da4a0278ecee5f71ba71
Dockins207 <otienodockins48@gmail.com>
2024-10-03 13:49:59 +0300

    push

--- components/StatisticsSection.js
+++ components/StatisticsSection.js
@@ -94,7 +94,7 @@ const StatisticsSection = () => {
       <div
         className={styles.statisticsBackground}
         style={{
-          backgroundImage: data?.image ? `url(${data.image})` : 'url(/path/to/default-image.jpg)',
+          backgroundImage: data?.image ? `url(${data.image})` : 'url(/logo/STATISTICS 2.svg)',
         }}
       >
         <h2>Current Statistics</h2>
