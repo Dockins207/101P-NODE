ffa1f76e8d6a0d808e9c1587899941fed6a3dce8
Dockins207 <otienodockins48@gmail.com>
2024-10-15 20:06:35 +0300

    push

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,13 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        {/* Add FontAwesome stylesheet */}
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        />
-        
-        {/* Add the favicon */}
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* Optionally, you can specify other favicon formats for different devices */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        {/* Link to favicon */}
+        <link rel="icon" href="/favicon.ico" /> {/* or /favicon.png */}
+        {/* Add any other metadata */}
       </Head>
       <body>
         <Main />
