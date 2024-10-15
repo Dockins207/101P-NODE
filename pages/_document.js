f0606e60c86ebbd935c5bbddda5a31dcc03169c0
Dockins207 <otienodockins48@gmail.com>
2024-10-15 19:47:56 +0300

    push

--- pages/_document.js
+++ pages/_document.js
@@ -14,9 +14,9 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
 
         {/* Optionally, you can specify other favicon formats for different devices */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.ico" />
+        <link rel="icon" type="image/ico" sizes="32x32" href="/favicon-32x32.ico" />
+        <link rel="icon" type="image/ic0" sizes="16x16" href="/favicon-16x16.ico" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
